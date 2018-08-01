7076bab8e05e23de938d0e5bf4d9cc1b4df1b675
David Gutiérrez Amarillas <daguam93@outlook.com>
2018-08-01 01:08:59 -0500

    Add resize to form

--- ImageProcessing/MenuForm.cs
+++ ImageProcessing/MenuForm.cs
@@ -15,17 +15,45 @@ namespace ImageProcessing
         Boolean drag;
         int mouseX;
         int mouseY;
+        private const int cGrip = 16;      // Grip size
+        private const int cCaption = 32;   // Caption bar height;
 
         public MenuForm()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
 
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(Brushes.DarkBlue, rc);
             ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
